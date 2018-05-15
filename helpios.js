2df5619900994cdc7b3f88d993e73cba58f1ac26
Mokayz <adrien.mokayz.warnke@gmail.com>
2018-05-15 16:01:02 +0200

    lolz

--- helpios.js
+++ helpios.js
@@ -1,1 +1,17 @@
+var button = document.createElement("Button");
+button.innerHTML = "Help";
+button.setAttribute("id","initPopup");
+button.style = "top:20;right:20;position:relative;z-index: 9999";
+button.addEventListener("click",initPopup(), false);
+document.body.appendChild(button);
 
+//var el = document.getElementById("initPopup");
+
+function initPopup(){
+
+$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
+
+}
